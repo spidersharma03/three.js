e629a8c0ca7ef90e525ca30e99e277a7ce3a2a99
Ben Houston <ben@exocortex.com>
2016-03-04 16:19:46 -0500

    centralizing encodings into encodings_pars_fragment.

--- src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl
@@ -76,3 +76,50 @@ vec4 LogLuvToLinear( in vec4 value ) {
   vec3 vRGB = Xp_Y_XYZp.rgb * cLogLuvInverseM;
   return vec4( max(vRGB, 0.0), 1.0 );
 }
+
+
+vec3 NormalToRGB( vec3 normal ) {
+	return 0.5 * normalize( vNormal ) + 0.5;
+}
+
+vec3 RGBToNormal( vec3 color ) {
+	return ( color.xyz - 0.5 ) * 2.0;
+}
+
+// this packs a value between 0 and 1 with linear precision into an RGBA 8-bit per channel buffer
+highp vec4 Float32ToRGBA(highp float f) {
+  highp float e =5.0;
+  highp float F = abs(f);
+  highp float Sign = step(0.0,-f);
+  highp float Exponent = floor(log2(F));
+  highp float Mantissa = (exp2(- Exponent) * F);
+  Exponent = floor(log2(F) + 127.0) + floor(log2(Mantissa));
+  highp vec4 rgba;
+  rgba[0] = 128.0 * Sign  + floor(Exponent*exp2(-1.0));
+  rgba[1] = 128.0 * mod(Exponent,2.0) + mod(floor(Mantissa*128.0),128.0);
+  rgba[2] = floor(mod(floor(Mantissa*exp2(23.0 -8.0)),exp2(8.0)));
+  rgba[3] = floor(exp2(23.0)*mod(Mantissa,exp2(-15.0)));
+  return rgba;
+}
+highp float RGBAToFloat32(highp vec4 rgba) {
+  highp float Sign = 1.0 - step(128.0,rgba[0])*2.0;
+  highp float Exponent = 2.0 * mod(rgba[0],128.0) + step(128.0,rgba[1]) - 127.0;
+  highp float Mantissa = mod(rgba[1],128.0)*65536.0 + rgba[2]*256.0 +rgba[3] + float(0x800000);
+  highp float Result =  Sign * exp2(Exponent) * (Mantissa * exp2(-23.0 ));
+  return Result;
+}
+
+// this packs a value between 0 and 1 with linear precision into an RGBA 8-bit per channel buffer
+
+vec4 UnitLinearToRGBA( const in float depth ) {
+ const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );
+ const vec4 bit_mask = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );
+ vec4 res = mod( depth * bit_shift * vec4( 255 ), vec4( 256 ) ) / vec4( 255 );
+ res -= res.xxyz * bit_mask;
+ return res;
+}
+float RGBAToUnitLinear( const in vec4 rgba_depth ) {
+  const vec4 bit_shift = vec4( 1.0 / ( 256.0 * 256.0 * 256.0 ), 1.0 / ( 256.0 * 256.0 ), 1.0 / 256.0, 1.0 );
+  float depth = dot( rgba_depth, bit_shift );
+  return depth;
+}
