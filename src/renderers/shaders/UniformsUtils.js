a45f6668ae46ae15818c0fc81086427c9f4b13c8
Ben Houston <ben@exocortex.com>
2016-05-13 14:39:38 -0400

    fix ShaderMaterial to copy uniform and defines upon creation.

--- src/renderers/shaders/UniformsUtils.js
+++ src/renderers/shaders/UniformsUtils.js
@@ -24,44 +24,50 @@ THREE.UniformsUtils = {
 
 	},
 
-	clone: function ( uniforms_src ) {
 
-		var uniforms_dst = {};
+		clone: function ( uniforms_src ) {
 
-		for ( var u in uniforms_src ) {
+			var uniforms_dst = {};
 
-			uniforms_dst[ u ] = {};
+			for ( var u in uniforms_src ) {
 
-			for ( var p in uniforms_src[ u ] ) {
+				uniforms_dst[ u ] = {};
 
-				var parameter_src = uniforms_src[ u ][ p ];
+				for ( var p in uniforms_src[ u ] ) {
 
-				if ( parameter_src instanceof THREE.Color ||
-					 parameter_src instanceof THREE.Vector2 ||
-					 parameter_src instanceof THREE.Vector3 ||
-					 parameter_src instanceof THREE.Vector4 ||
-					 parameter_src instanceof THREE.Matrix3 ||
-					 parameter_src instanceof THREE.Matrix4 ||
-					 parameter_src instanceof THREE.Texture ) {
+					var parameter_src = uniforms_src[ u ][ p ];
 
-					uniforms_dst[ u ][ p ] = parameter_src.clone();
-
-				} else if ( Array.isArray( parameter_src ) ) {
-
-					uniforms_dst[ u ][ p ] = parameter_src.slice();
-
-				} else {
-
-					uniforms_dst[ u ][ p ] = parameter_src;
+					uniforms_dst[ u ][ p ] = this.cloneValue( parameter_src );
 
 				}
 
 			}
 
+			return uniforms_dst;
+
+		},
+
+		cloneValue: function( parameter_src ) {
+
+			if ( parameter_src instanceof THREE.Color ||
+				 parameter_src instanceof THREE.Vector2 ||
+				 parameter_src instanceof THREE.Vector3 ||
+				 parameter_src instanceof THREE.Vector4 ||
+				 parameter_src instanceof THREE.Matrix3 ||
+				 parameter_src instanceof THREE.Matrix4 ||
+				 parameter_src instanceof THREE.Texture ) {
+
+				return parameter_src.clone();
+
+			} else if ( Array.isArray( parameter_src ) ) {
+
+				return parameter_src.slice();
+
+			} else {
+
+				return parameter_src;
+
+			}
+
 		}
-
-		return uniforms_dst;
-
-	}
-
 };
