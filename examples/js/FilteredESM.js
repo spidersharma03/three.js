158cc9fd21ea9445a184d74a72ac7a4409f7b2ce
Prashant Sharma <prashantsharma@Prashants-MacBook-Pro.local>
2016-10-26 11:09:39 +0530

    Progressive ESM First commit

--- examples/js/FilteredESM.js
+++ examples/js/FilteredESM.js
@@ -22,6 +22,10 @@ function FilteredESM( scene, camera, light ) {
   this.shadowDepthMap.generateMipmaps = false;
   this.shadowDepthMapTemp.generateMipmaps = false;
 
+  var params = { format: THREE.RGBAFormat, minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter};
+  this.shadowBuffer     = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, params);
+  this.shadowBufferTemp = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, params);
+
   this.depthMaterial      = this.getShadowDepthWriteMaterial();
   this.depthMaterial.uniforms["nearFarPlanes"].value = new THREE.Vector2(nearPlane, farPlane);
 
@@ -29,18 +33,59 @@ function FilteredESM( scene, camera, light ) {
   this.shadowFilterMaterial.uniforms["shadowMapSize"].value = new THREE.Vector2(shadowMapWidth, shadowMapHeight);
   this.VERTICAL_DIRECTION = new THREE.Vector2(0, 1);
   this.HORIZONTAL_DIRECTION = new THREE.Vector2(1, 0);
+  this.shadowFilterMaterial.defines["KERNEL_WIDTH"] = 1;
+
+  this.accumulatePassMaterial = this.getAccumulatePassMaterial();
+  this.accumulatePassMaterial.uniforms["windowSize"].value = new THREE.Vector2(window.innerWidth, window.innerHeight);
+  this.accumulatePassMaterial.uniforms["shadowMap"].value = this.shadowDepthMap;
+  this.accumulatePassMaterial.uniforms["nearFarPlanes"].value = new THREE.Vector2(nearPlane, farPlane);
 
   this.finalPassMaterial = this.getFinalPassMaterial();
   this.finalPassMaterial.uniforms["windowSize"].value = new THREE.Vector2(window.innerWidth, window.innerHeight);
-  this.finalPassMaterial.uniforms["shadowMap"].value = this.shadowDepthMap;
-  this.finalPassMaterial.uniforms["nearFarPlanes"].value = new THREE.Vector2(nearPlane, farPlane);
+  this.accumulatePassMaterial.uniforms["shadowBuffer"].value = this.getShadowBuffer();
+
+  this.frameCount = 0;
+  this.poissonSampler = new PoissonDiskGenerator(500, -1);
+  this.lightPositionSamples = this.poissonSampler.generatePoints();
+  this.currentLightSample = new THREE.Vector3();
+  this.lightOrientation   = new THREE.Vector3(0, -1, 0);
+  this.lightTarget = new THREE.Vector3();
+  this.accumulatePassMaterial.uniforms["maxSamples"].value = this.lightPositionSamples.length;
+  this.arealightSize = 4;
 }
 
 FilteredESM.prototype = {
 
   constructor: FilteredESM,
 
+  getShadowBuffer: function() {
+   return this.frameCount %2 === 0 ? this.shadowBufferTemp : this.shadowBuffer;
+  },
+
+  sampleLight: function( light, sampleNumber ) {
+    var lightWorldMatrix = light.matrixWorld;
+    if( sampleNumber < this.lightPositionSamples.length ) {
+      var sample = this.lightPositionSamples[sampleNumber];
+      this.currentLightSample.x = sample.x * this.arealightSize;
+      this.currentLightSample.y = 0.0;
+      this.currentLightSample.z = sample.y * this.arealightSize;
+    } else {
+      var sample = this.lightPositionSamples[0];
+      this.currentLightSample.x = sample.x * this.arealightSize;
+      this.currentLightSample.y = 0.0;
+      this.currentLightSample.z = sample.y * this.arealightSize;
+    }
+    this.currentLightSample.applyMatrix4(lightWorldMatrix);
+    this.lightOrientation.set(0, -1, 0);
+    this.lightOrientation.transformDirection(lightWorldMatrix);
+    return this.currentLightSample;
+  },
+
   render: function( renderer ) {
+
+    this.frameCount = this.frameCount >= this.lightPositionSamples.length ? this.lightPositionSamples.length : this.frameCount;
+
+    this.frameCount++;
     // 1. Render depth to shadow map
     this.renderShadowDepthMap( renderer, this.light, this.shadowDepthMap );
 
@@ -61,20 +106,35 @@ FilteredESM.prototype = {
     );
     shadowMatrix.multiply( this.lightCamera.projectionMatrix );
     shadowMatrix.multiply( this.lightCamera.matrixWorldInverse );
-    var matrix = this.finalPassMaterial.uniforms["shadowMatrix"].value;
+    var matrix = this.accumulatePassMaterial.uniforms["shadowMatrix"].value;
     matrix.copy(shadowMatrix);
 
-    // this.finalPassMaterial.uniforms["shadowBuffer"].value = this.getShadowBuffer();
+    this.accumulatePassMaterial.uniforms["currentFrameCount"].value = this.frameCount;
+    var currentShadowReadTarget = (this.frameCount % 2 == 0) ? this.shadowBuffer : this.shadowBufferTemp;
+    this.accumulatePassMaterial.uniforms["shadowBuffer"].value = currentShadowReadTarget;
+
+    this.accumulatePassMaterial.uniforms["lightPosition"].value = this.light.position;
+    this.scene.overrideMaterial = this.accumulatePassMaterial;
+
+    var currentShadowWriteTarget = (this.frameCount % 2 == 0) ? this.shadowBufferTemp : this.shadowBuffer;
+
+    renderer.render( this.scene, this.camera, currentShadowWriteTarget, true);
+    this.scene.overrideMaterial = null;
+
+    this.finalPassMaterial.uniforms["shadowBuffer"].value = this.shadowBuffer;
     this.finalPassMaterial.uniforms["lightPosition"].value = this.light.position;
     this.scene.overrideMaterial = this.finalPassMaterial;
     renderer.render( this.scene, this.camera, null, true );
     this.scene.overrideMaterial = null;
+
   },
   // Render shadow map of a light
   renderShadowDepthMap: function( renderer, light, shadowDepthMap ) {
-    var lightPosition = light.position;
+    var lightPosition = this.sampleLight( light, this.frameCount - 1);
     this.lightCamera.position.copy(lightPosition);
-    this.lightCamera.lookAt(light.target.position);
+    this.lightTarget.copy(lightPosition);
+    this.lightTarget.addScaledVector(this.lightOrientation, 1000);
+    this.lightCamera.lookAt(this.lightTarget);
     // this.lightCamera.lookAt(light.target.position);
     this.lightCamera.updateMatrixWorld();
     this.scene.overrideMaterial = this.depthMaterial;
@@ -118,6 +178,9 @@ FilteredESM.prototype = {
 
   getShadowFilterMaterial: function( ) {
     return new THREE.ShaderMaterial( {
+      defines: {
+        "KERNEL_WIDTH" : 1
+      },
 
 			uniforms: {
         "shadowMap" : { value: null },
@@ -136,7 +199,6 @@ FilteredESM.prototype = {
         uniform sampler2D shadowMap;\
         uniform vec2 shadowMapSize;\
         uniform vec2 direction;\
-        const int KERNEL_WIDTH = 3;\
         const float SIGMA = float(KERNEL_WIDTH);\
         \
         float gaussianPdf(in float x, in float sigma) {\
@@ -204,15 +266,18 @@ FilteredESM.prototype = {
     } );
   },
 
-  getFinalPassMaterial: function() {
+  getAccumulatePassMaterial: function() {
     return new THREE.ShaderMaterial( {
 
 			uniforms: {
         "shadowMap" : { value: null },
+        "shadowBuffer" : { value: null },
         "windowSize"   : { value: null },
         "shadowMatrix": { value: new THREE.Matrix4() },
         "lightPosition": { value: new THREE.Vector3() },
-        "nearFarPlanes" : { value: new THREE.Vector2() }
+        "nearFarPlanes" : { value: new THREE.Vector2() },
+        "currentFrameCount" : { value: 0 },
+        "maxSamples" : { value: 0}
 			},
 
       vertexShader: "varying vec3 normalEyeSpace;\
@@ -236,7 +301,10 @@ FilteredESM.prototype = {
         varying vec4 shadowCoord;\
         uniform vec2 windowSize;\
         uniform sampler2D shadowMap;\
+        uniform sampler2D shadowBuffer;\
         uniform vec2 nearFarPlanes;\
+        uniform float currentFrameCount;\
+        uniform float maxSamples;\
         void main() {\
           float lightDepth = shadowCoord.z + 2.0*nearFarPlanes.y*nearFarPlanes.x/(nearFarPlanes.y - nearFarPlanes.x);\
 		      lightDepth *= -((nearFarPlanes.y - nearFarPlanes.x)/(nearFarPlanes.y + nearFarPlanes.x));\
@@ -250,8 +318,48 @@ FilteredESM.prototype = {
           const float c = 0.75;\
           float shadowValue = step( lightDist, shadowDepth );\
  			    shadowValue = min(exp(-c*(lightDist - shadowDepth)), 1.0);\
-          gl_FragColor = vec4(NdotL * shadowValue);\
+          float prevShadow = unpackRGBAToDepth(texture2D( shadowBuffer, gl_FragCoord.xy/windowSize ));\
+          if(currentFrameCount > maxSamples){\
+            shadowValue = prevShadow;\
+          }\
+          float newShadowValue = (currentFrameCount - 1.0) * prevShadow + shadowValue;\
+          newShadowValue /= currentFrameCount;\
+          \
+          gl_FragColor = packDepthToRGBA(newShadowValue);\
         }",
     } );
+  },
+
+  getFinalPassMaterial: function() {
+    return new THREE.ShaderMaterial( {
+
+    			uniforms: {
+            "shadowBuffer" : { value: null },
+            "windowSize"   : { value: null },
+            "lightPosition": { value: new THREE.Vector3() }
+    			},
+
+          vertexShader: "varying vec3 normalEyeSpace;\
+          varying vec3 lightVector;\
+          uniform vec3 lightPosition;\
+          void main() {\
+            normalEyeSpace = normalize(normalMatrix * normal);\
+            vec3 lightPositionEyeSpace = (viewMatrix * vec4(lightPosition, 1.0)).xyz;\
+            lightVector = lightPositionEyeSpace - (modelViewMatrix * vec4( position, 1.0 )).xyz;\
+            lightVector = normalize(lightVector);\
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n\
+          }",
+
+          fragmentShader: "#include <packing>\
+            varying vec3 normalEyeSpace;\
+            varying vec3 lightVector;\
+            uniform vec2 windowSize;\
+            uniform sampler2D shadowBuffer;\
+            void main() {\
+              float NdotL = max(dot( normalize(normalEyeSpace), normalize(lightVector)), 0.0);\
+              float shadowValue = unpackRGBAToDepth(texture2D( shadowBuffer, gl_FragCoord.xy/windowSize));\
+              gl_FragColor = vec4(shadowValue * NdotL);\
+            }",
+        } );
   }
 }
