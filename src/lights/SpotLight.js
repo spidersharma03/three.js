6df9adf13c4d121a45e388a066874e7f27379cdf
godlzr <godlzr@gmail.com>
2017-03-09 16:53:12 -0500

    modify autoshadow method

--- src/lights/SpotLight.js
+++ src/lights/SpotLight.js
@@ -1,7 +1,8 @@
 import { Light } from './Light';
 import { SpotLightShadow } from './SpotLightShadow';
 import { Object3D } from '../core/Object3D';
-
+import { Box3 } from '../math/Box3';
+import { Vector3 } from '../math/Vector3';
 /**
  * @author alteredq / http://alteredqualia.com/
  */
@@ -16,6 +17,7 @@ function SpotLight( color, intensity, distance, angle, penumbra, decay ) {
 	this.updateMatrix();
 
 	this.target = new Object3D();
+	this.autoShadow = false;
 
 	Object.defineProperty( this, 'power', {
 		get: function () {
@@ -64,20 +66,19 @@ SpotLight.prototype = Object.assign( Object.create( Light.prototype ), {
 
 } );
 
-SpotLight.prototype.autoShadow = function ( scene ) {
+SpotLight.prototype.createAutoShadow = function ( scene ) {
 
 	if (!scene) return;
 
 	var position = this.position;
 	var targetPosition = this.target.position;
 
-  	const box = new THREE.Box3();
-  	const result = new THREE.Box3();
+  	const box = new Box3();
+  	const result = new Box3();
   	result.makeEmpty();
-
 	scene.traverseVisible(function(object) {
 
-		if(object instanceof THREE.Mesh) {
+		if( object.isMesh ) {
 
 		    var geometry = object.geometry;
 
@@ -97,17 +98,20 @@ SpotLight.prototype.autoShadow = function ( scene ) {
 		}
 
 	});
-
 	var boundingSphere = result.getBoundingSphere();
-    var vector1 = new THREE.Vector3().subVectors( targetPosition, position );
-    var vector2 = new THREE.Vector3().subVectors( boundingSphere.center, position );
+    var vector1 = new Vector3().subVectors( targetPosition, position );
+    if (this.children[0] && this.children[0].uuid === this.target.uuid) {
+    	vector1 =  new Vector3(0, 0, -1).applyQuaternion(this.quaternion);
+    }
+    var vector2 = new Vector3().subVectors( boundingSphere.center, position );
     var distance = boundingSphere.center.distanceTo(position);
     var angle = vector1.angleTo(vector2);
     var far = angle > (Math.PI / 2) ? 0 : distance * Math.cos(angle) + boundingSphere.radius;
     var near = (far === 0 || (far - boundingSphere.radius * 2) < 0 )? 0.01 : far - boundingSphere.radius * 2;
-
+    this.distance = far;
     this.shadow.camera.far = far;
     this.shadow.camera.near = near;
+    this.shadow.camera.updateProjectionMatrix();
 }
 
 export { SpotLight };
