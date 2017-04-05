6bcd16f166141e21d9526d5d070e1819d7f2e874
Ben Houston <ben@exocortex.com>
2017-04-05 16:18:51 -0400

    allow for RenderPass to render clean and composite over.

--- examples/js/postprocessing/RenderPass.js
+++ examples/js/postprocessing/RenderPass.js
@@ -1,5 +1,6 @@
 /**
  * @author alteredq / http://alteredqualia.com/
+ * @author bhouston / http://clara.io/
  */
 
 THREE.RenderPass = function ( scene, camera, overrideMaterial, clearColor, clearAlpha ) {
@@ -8,6 +9,8 @@ THREE.RenderPass = function ( scene, camera, overrideMaterial, clearColor, clear
 
 	this.scene = scene;
 	this.camera = camera;
+
+	this.renderOver = false;
 
 	this.overrideMaterial = overrideMaterial;
 
@@ -18,7 +21,25 @@ THREE.RenderPass = function ( scene, camera, overrideMaterial, clearColor, clear
 	this.clearDepth = false;
 	this.needsSwap = false;
 
+	if ( THREE.CopyShader === undefined ) console.error( "THREE.SSAARenderPass relies on THREE.CopyShader" );
+
+	this.overMaterial = new THREE.ShaderMaterial( THREE.CopyShader );
+	this.overMaterial.uniforms = THREE.UniformsUtils.clone( this.overMaterial.uniforms );
+	this.overMaterial.blending = THREE.NormalBlending;
+	this.overMaterial.premultipliedAlpha = true;
+	this.overMaterial.transparent = true;
+	this.overMaterial.depthTest = false;
+	this.overMaterial.depthWrite = false;
+
+	this.camera2 = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+	this.scene2	= new THREE.Scene();
+	this.quad2 = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), this.copyMaterial );
+	this.quad2.frustumCulled = false; // Avoid getting clipped
+	this.scene2.add( this.quad2 );
+	
 };
+
+
 
 THREE.RenderPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
 
@@ -32,23 +53,41 @@ THREE.RenderPass.prototype = Object.assign( Object.create( THREE.Pass.prototype 
 		this.scene.overrideMaterial = this.overrideMaterial;
 
 		var oldClearColor, oldClearAlpha;
-
-		if ( this.clearColor !== undefined ) {
-
-			oldClearColor = renderer.getClearColor();
-			oldClearAlpha = renderer.getClearAlpha();
-
-			renderer.setClearColor( this.clearColor, this.clearAlpha );
-
-		}
+		oldClearColor = renderer.getClearColor();
+		oldClearAlpha = renderer.getClearAlpha();
 
 		if ( this.clearDepth ) {
 
 			renderer.clearDepth();
 
 		}
+	
+		if( this.renderOver ) {
 
-		renderer.renderOverride( this.overrideMaterial, this.scene, this.camera, this.renderToScreen ? null : readBuffer, this.clear );
+			renderer.setClearColor( 0x000000, 0 );
+			renderer.renderOverride( this.overrideMaterial, this.scene, this.camera, writeBuffer, true );
+
+			this.overMaterial.uniforms[ 'tDiffuse' ].value = writeBuffer.texture;
+
+			if ( this.clearColor !== undefined ) {
+	
+				renderer.setClearColor( this.clearColor, this.clearAlpha );
+
+			}
+
+			renderer.renderOverride( this.overMaterial, this.scene2, this.camera2, this.renderToScreen ? null : readBuffer, this.clear );
+		}
+		else {
+
+			if ( this.clearColor !== undefined ) {
+	
+				renderer.setClearColor( this.clearColor, this.clearAlpha );
+
+			}
+			renderer.renderOverride( this.overrideMaterial, this.scene, this.camera, this.renderToScreen ? null : readBuffer, this.clear );
+
+		}
+
 
 		if ( this.clearColor ) {
 
