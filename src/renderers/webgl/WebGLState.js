491111e6ebe716056dc2322e75a9c11968d12cd1
Ben Houston <ben@exocortex.com>
2017-04-06 16:19:41 -0400

    fix bug in alpha blending of pre-multiplied alpha.

--- src/renderers/webgl/WebGLState.js
+++ src/renderers/webgl/WebGLState.js
@@ -570,7 +570,7 @@ function WebGLState( gl, extensions, paramThreeToGL ) {
 				if ( premultipliedAlpha ) {
 
 					gl.blendEquationSeparate( gl.FUNC_ADD, gl.FUNC_ADD );
-					gl.blendFuncSeparate( gl.ONE, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA );
+					gl.blendFuncSeparate( gl.ONE, gl.ONE_MINUS_SRC_ALPHA, gl.ONE_MINUS_DST_ALPHA, gl.ONE );
 
 				} else {
 
